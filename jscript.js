396ff5d7fe1eea6f8d5d0d15f98869a01f7a200e
Soujanya Geddam <sgeddam@utexas.edu>
2022-07-12 21:15:58 -0500

    Add event listeners for mouseover

--- jscript.js
+++ jscript.js
@@ -10,4 +10,14 @@ function createGrid() {
     }
 }
 
+function changeColor() {
+    const cells = document.querySelectorAll('.cell');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("mouseover", () => {
+            cells[i].classList.add('fill');
+        });
+    }
+}
+
 createGrid();
+changeColor();
